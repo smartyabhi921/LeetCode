e13fb317ea79345f32464a92ca1f6b3c2f808b6a
ABHINAV KUMAR SINGH <125972398+smartyabhi921@users.noreply.github.com>
2024-03-31 21:56:48 +0530

    Update Leet2.java
    changed

--- Leet2.java
+++ Leet2.java
@@ -1,27 +1,24 @@
-import java.util.*;
-
 class Solution {
-    public int countSubarrays(int[] nums, int minK, int maxK) {
-        int result = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-            for (int j = i; j < nums.length; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-                if (min == minK && max == maxK)
-                    result++;
-            }
-        }
-        return result;
-    }
-}
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        long res = 0;
+        int badIdx = -1, leftIdx = -1, rightIdx = -1;
 
-public class Leet2 {
-    public static void main(String[] args) {
-        int[] nums = { 1, 3, 5, 2, 7, 5 };
-        int minK = 1;
-        int maxK = 5;
-        System.out.println(new Solution().countSubarrays(nums, minK, maxK)); // Output: 2
+        for (int i = 0; i < nums.length; ++i) {
+            if (!(minK <= nums[i] && nums[i] <= maxK)) {
+                badIdx = i;
+            }
+
+            if (nums[i] == minK) {
+                leftIdx = i;
+            }
+
+            if (nums[i] == maxK) {
+                rightIdx = i;
+            }
+
+            res += Math.max(0, Math.min(leftIdx, rightIdx) - badIdx);
+        }
+
+        return res;
     }
 }
